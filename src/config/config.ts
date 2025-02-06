b1908ab8aa93e79f0f725f2ab45162f2e674be1e
Fernando Parra <thefer767@gmail.com>
2025-02-06 13:36:47 -0400

    Fist Commit

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+export default {
+    jwtSecret:process.env.JWT_SECRET || 'somesecret',
+    DB:{
+        URI: process.env.MONGODB_URI || 'mongodb://localhost/BackendPF',
+        USER: process.env.MONGODB_USER || '',
+        PASSWORD: process.env.MONGODB_PASSWORD || ''
+    }
+}
